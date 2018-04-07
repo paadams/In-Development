228c8d3397505ea2249b7f23796d74f964226dc0
Peter Adams <paadams@gmail.com>
2018-04-06 21:10:21 -0500

    updates

--- Configure vDS Portgroups/CreateNewVDPortgroups.ps1
+++ Configure vDS Portgroups/CreateNewVDPortgroups.ps1
@@ -1,6 +1,6 @@
 
 
-$MyVLANFile = Import-CSV C:\Temp\vss_new_portgroups.csv
+$MyVLANFile = Import-CSV C:\Temp\new_vds_portgroups.csv
 
 ForEach ($VLAN in $MyVLANFile) {
         $MyvSwitch = $VLAN.vDS
@@ -9,5 +9,8 @@ ForEach ($VLAN in $MyVLANFile) {
 
       
 
-        Get-VDSwitch -Name $MyvSwitch | New-VDPortgroup -Name $MyVLANname -VlanId $MyVLANid        }
+        Get-VDSwitch -Name $MyvSwitch | New-VDPortgroup -Name $MyVLANname -VlanId $MyVLANid    
         
+        
+    
+} # End ForEach
