adeb57e498c227c4ab21865977522b8f6e1144ce
Peter Adams <paadams@gmail.com>
2018-08-16 06:33:33 -0500

    Updates

---
+++ Invoke-vCenterVM.ps1
@@ -0,0 +1,16 @@
+﻿
+$Credential = Get-Credential
+$auth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($Credential.UserName+':'+$Credential.GetNetworkCredential().Password))
+$head = @{
+  'Authorization' = "Basic $auth"
+}
+
+	
+$r = Invoke-WebRequest -Uri https://192.168.1.162/rest/com/vmware/cis/session -Method Post -Headers $head
+$token = (ConvertFrom-Json $r.Content).value
+$session = @{'vmware-api-session-id' = $token}
+
+$r1 = Invoke-WebRequest -Uri https://192.168.1.162/rest/vcenter/vm -Method Get -Headers $session
+$vms = (ConvertFrom-Json $r1.Content).value
+$vms
+$vms.Count
